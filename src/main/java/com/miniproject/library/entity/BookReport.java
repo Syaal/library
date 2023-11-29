c022332f284bf1511d572999ab525bdb519472c7
abdulqowi <93390507+abdulqowi@users.noreply.github.com>
2023-11-29 17:07:57 +0700

    Buat id biar bisa buat table

--- src/main/java/com/miniproject/library/entity/BookReport.java
+++ src/main/java/com/miniproject/library/entity/BookReport.java
@@ -6,5 +6,7 @@ import lombok.Data;
 @Entity
 @Data
 public class BookReport {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }
--- src/main/java/com/miniproject/library/entity/ReturnBook.java
+++ src/main/java/com/miniproject/library/entity/ReturnBook.java
@@ -1,9 +1,18 @@
 package com.miniproject.library.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.util.Date;
-
+@Entity
+@Data
 public class ReturnBook {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Date pengembalian;
     private BigDecimal denda;
