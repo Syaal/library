65ce121cac95debb9bd21fa817bb5dcfeba37722
Adytia Dwi Hermawan <adityadwi1109@gmail.com>
2023-12-17 12:15:14 +0700

    Menghapus entity, dto, dan repository yang tidak digunakan

--- src/main/java/com/miniproject/library/repository/VisitorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.miniproject.library.repository;
-
-import com.miniproject.library.entity.Visitor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VisitorRepository extends JpaRepository<Visitor, Integer> {
-}
