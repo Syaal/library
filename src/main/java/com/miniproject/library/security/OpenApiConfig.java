5c9f59fec7ae6c4909329f05201c9b937feecd08
Adytia Dwi Hermawan <adityadwi1109@gmail.com>
2023-12-17 14:39:12 +0700

    Open Api Config swagger

---
+++ src/main/java/com/miniproject/library/security/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.miniproject.library.security;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        info = @Info(
+                description = "OpenApi documentation for Library Management Service",
+                title = "OpenApi for Library Management Service"
+        ),
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT Auth Description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+
+public class OpenApiConfig {
+}
